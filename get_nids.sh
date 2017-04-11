3cc002e4eea5459a0ccce33d05b0625b6d0599ac
devnoname120 <devnoname120+gh@gmail.com>
2017-04-11 15:29:36 +0200

    Add support for another module dump name format

--- get_nids.sh
+++ get_nids.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+shopt -s nullglob
+
 if [ "$#" -lt 1 ]; then
 	echo -e "Usage:\n\t$0 directory"
 	exit 1
@@ -15,11 +17,30 @@ if [ ! -d "$1" ]; then
 	exit 1
 fi
 
-for file in "$1"/*_0.bin
-do
-	parts=($(basename "$file" | sed -rn "s/0x[0-9A-Fa-f]+_(0x[0-9A-Fa-f]+)_(\w+)_0\.bin/\1 \2/p"))
-	name=${parts[1]}
-	addr=${parts[0]}
 
-	vita-nid-finder-flow "$file" "$name" "$addr"
+function folder_nids {
+	for file in "$1"/*_0.bin
+	do
+		parts=($(basename "$file" | sed -rn "s/0x[0-9A-Fa-f]+_(0x[0-9A-Fa-f]+)_(\w+)_0\.bin/\1 \2/p"))
+		name=${parts[1]}
+		addr=${parts[0]}
+
+		vita-nid-finder-flow "$file" "$name" "$addr"
+	done
+
+	for file in "$1"/*_seg0.bin
+	do
+		parts=($(basename "$file" | sed -rn "s/(\w+)_(0x[0-9A-Fa-f]+)_seg0.bin/\2 \1/p"))
+		name=${parts[1]}
+		addr=${parts[0]}
+
+		vita-nid-finder-flow "$file" "$name" "$addr"
+	done
+}
+
+
+for folder in "$@"
+do
+	folder_nids "$folder"
 done
+
